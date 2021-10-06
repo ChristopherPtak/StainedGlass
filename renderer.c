defce78acd1e0d505537b31f8f845c7713b08781
Christopher Ptak <cptak@comcast.net>
2021-10-06 01:05:15 -0400

    Connected renderer to the HTML canvas

--- renderer.c
+++ renderer.c
@@ -1,9 +1,11 @@
 
 #include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #include <emscripten.h>
 
-void render_Mandelbrot(size_t dimx, size_t dimy, float* array,
+void render_Mandelbrot(size_t dimx, size_t dimy, uint8_t* array,
                        double posx, double posy, double scale)
 {
     size_t i;
@@ -11,7 +13,11 @@ void render_Mandelbrot(size_t dimx, size_t dimy, float* array,
 
     for (i = 0; i < dimx; ++i) {
         for (j = 0; j < dimy; ++j) {
-            array[(dimx * i) + j] = 1.0;
+            size_t offset = 4 * ((dimx * i) + j);
+            array[offset + 0] = 0;
+            array[offset + 1] = 0;
+            array[offset + 2] = 0;
+            array[offset + 3] = 255;
         }
     }
 }
